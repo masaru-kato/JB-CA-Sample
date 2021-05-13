6fcd9c19ff759e8c44ea3a26fd8b23a9e7c1af25
masaru.kato <masaru.kato@salesforce.com>
2021-05-14 00:16:25 +0900

    add outargs

--- routes/activity.js
+++ routes/activity.js
@@ -190,8 +190,7 @@ exports.execute = function (req, res) {
   logData(req);
   console.log(`OUT ARGS: ${JSON.stringify(outArgs)}`);
   //res.send(200, 'Execute');
-  return res.status(200).json(JSON.stringify(outArgs));
-
+  return res.status(200).send(JSON.stringify(outArgs));
 };
 
 
